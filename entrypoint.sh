76f7a34845b25003f2a9a133addb4bb187351d85
Alexandre Abidri <alex.abidri@gmail.com>
2022-11-26 19:26:56 +0800

    TECH Remove build project

--- entrypoint.sh
+++ entrypoint.sh
@@ -141,8 +141,8 @@ function docker_build() {
     INPUT_EXTRA_BUILD_ARGS="$INPUT_EXTRA_BUILD_ARGS --cache-from=$INPUT_CACHE_FROM"
   fi
 
-  echo "docker buildx build --build-arg project=web --platform linux/arm64 -f $INPUT_DOCKERFILE $docker_tag_args $INPUT_PATH"
-  docker buildx build --build-arg project=web --platform linux/arm64 -f $INPUT_DOCKERFILE $docker_tag_args $INPUT_PATH
+  echo "docker buildx build --platform linux/arm64 -f $INPUT_DOCKERFILE $docker_tag_args $INPUT_PATH"
+  docker buildx build --platform linux/arm64 -f $INPUT_DOCKERFILE $docker_tag_args $INPUT_PATH
   echo "== FINISHED DOCKERIZE"
 }
 
